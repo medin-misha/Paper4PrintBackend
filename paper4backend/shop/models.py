ce16aba2e04e5581362b2813522ab59b089402b4
misha <medinskijmisa228@gmail.com>
2025-06-25 16:37:26 +0300

    added Order model

--- paper4backend/shop/models.py
+++ paper4backend/shop/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+
+class OrderStatusChoices(models.TextChoices):
+    CREATED = "CREATED", "Created"
+    PAID = "PAID", "Paid"
+    PROCESSED = "PROCESSED", "Processed"
+    SUCCESS = "SUCCESS", "Success"
+    CANCELLED = "CANCELLED", "Cancelled"
+    FAILED = "FAILED", "Failed"
+    RETURNED = "RETURNED", "Returned"
+
 
 # Create your models here.
+class Orders(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(to=User, related_name="orders", on_delete=models.CASCADE)
+    # CREATED, PAID, PROCESSED, SUCCESS, CANCELLED, FAILED
+    status = models.CharField(
+        null=False,
+        default=OrderStatusChoices.CREATED,
+        choices=OrderStatusChoices.choices,
+    )
+    sender_service = models.CharField(null=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"Order {self.uuid} - {self.user.username} - {self.status}"
