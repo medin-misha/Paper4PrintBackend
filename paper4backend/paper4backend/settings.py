90ec4ef9feee7b8b286208633a15a664e90b2a98
misha <medinskijmisa228@gmail.com>
2025-07-01 14:26:08 +0300

    add rest_framework

--- paper4backend/paper4backend/settings.py
+++ paper4backend/paper4backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "storages",
     "shop.apps.ShopConfig",
     "paper4auth.apps.Paper4AuthConfig",
