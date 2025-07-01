2e40fffc034405b80d3fadd1c612972d01dba7d6
misha <medinskijmisa228@gmail.com>
2025-07-01 13:36:36 +0300

    add profile model

--- paper4backend/paper4auth/models.py
+++ paper4backend/paper4auth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Profile(User):
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    phone = models.CharField(null=True, blank=True)
+    chat_id = models.CharField(null=False, blank=False)
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="profiling")
