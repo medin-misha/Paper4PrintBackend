29931a0a4f4d39ac5fbd4c2b9c6850276ba22088
misha <medinskijmisa228@gmail.com>
2025-07-02 13:20:20 +0300

    added and customized admin panel

--- paper4backend/shop/admin.py
+++ paper4backend/shop/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import ProductImage, Orders, Products, ProductToOrder
+from .models import ProductImage, Orders, Products, ProductToOrder, Tags
 
 
 class ProductToOrderInline(admin.TabularInline):
     extra = 1
     model = ProductToOrder
+
+class ProductImageInline(admin.TabularInline):
+    extra = 1
+    model = ProductImage
 
 
 @admin.register(ProductImage)
@@ -16,14 +20,24 @@ class ProductImageAdmin(admin.ModelAdmin):
 class OrderModelAdmin(admin.ModelAdmin):
     list_display = ["status", "user", "created_timestamp"]
     inlines = [ProductToOrderInline]
+    list_filter = ["status", "user"]
+    ordering = ["-created_timestamp", "status"]
 
 
 @admin.register(Products)
 class ProductsModelAdmin(admin.ModelAdmin):
-    list_display = ["name", "created_at"]
-    inlines = [ProductToOrderInline]
+    list_display = ["name", "price", "created_at", "updated_at"]
+    readonly_fields = ["created_at", "updated_at"]
+    inlines = [ProductImageInline,]
+    filter_horizontal = ["tags",]
+    search_fields = ["name", "price", "description"]
 
 
 @admin.register(ProductToOrder)
 class ProductToOrder(admin.ModelAdmin):
-    pass
+    search_fields = ["product", "order"]
+
+@admin.register(Tags)
+class TagsModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "slag"]
+    search_fields = ["name", "slag"]
